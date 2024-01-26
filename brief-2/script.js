20283e589fd684250a012a4937b0add3f73d714c
OrhanMA <orhan.madi.assani@gmail.com>
2024-01-26 22:30:30 +0100

    added comment on script for animation repetition in intersection observer

--- brief-2/script.js
+++ brief-2/script.js
@@ -6,6 +6,12 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     }
+    /**
+     * le else permet de repeter l'animation a chaque apparition de l'element dans le viewport
+     */
+    // else {
+    //   entry.target.classList.remove("show");
+    // }
   });
 });
 
