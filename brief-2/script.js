5e5fbcedb0a7a9ffaf3fbd3a12692f84d3603cbd
OrhanMA <orhan.madi.assani@gmail.com>
2024-01-28 18:51:53 +0100

    hero and header for brief-2

--- brief-2/script.js
+++ brief-2/script.js
@@ -18,3 +18,43 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 
 hiddenElements.forEach((element) => observer.observe(element));
+
+const video = document.querySelector(".video-container video");
+const texts = document.querySelectorAll(".text");
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+const observerVideo = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const videoRect = video.getBoundingClientRect();
+    const textRect = entry.target.getBoundingClientRect();
+
+    const isIntersecting = !(
+      textRect.right < videoRect.left ||
+      textRect.left > videoRect.right ||
+      textRect.bottom < videoRect.top ||
+      textRect.top > videoRect.bottom
+    );
+
+    if (isIntersecting) {
+      entry.target.classList.add("intersecting");
+    } else {
+      entry.target.classList.remove("intersecting");
+    }
+  });
+}, options);
+
+texts.forEach((text) => observerVideo.observe(text));
+
+const header = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    header.classList.add("isOnScroll");
+  } else if (window.scrollY <= 100) {
+    header.classList.remove("isOnScroll");
+  }
+});
